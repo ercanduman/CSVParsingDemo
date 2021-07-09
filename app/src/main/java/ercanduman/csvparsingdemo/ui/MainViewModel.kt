4d511afb95b7f6facde8bd4ea86c0cae0f78ea7b
Ercan Duman <ercan.duman@macellan.net>
2021-07-09 17:02:40 +0300

    MainViewModel.kt created to store and keep data for UI

---
+++ app/src/main/java/ercanduman/csvparsingdemo/ui/MainViewModel.kt
@@ -0,0 +1,27 @@
+package ercanduman.csvparsingdemo.ui
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ercanduman.csvparsingdemo.data.source.LocalDataSource
+import ercanduman.csvparsingdemo.util.Resource
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+/**
+ * ViewModel is responsible to retrieve data and keep it for UI. ViewModel is lifecycle aware component
+ * and survives configuration changes.
+ *
+ * @author ercanduman
+ * @since  09.07.2021
+ */
+class MainViewModel(private val dataSource: LocalDataSource) : ViewModel() {
+
+    private val _resourceMutableLiveData = MutableLiveData<Resource>()
+    val resourceLiveData: LiveData<Resource> = _resourceMutableLiveData
+
+    fun getData() = viewModelScope.launch {
+        dataSource.readFile().collect { _resourceMutableLiveData.value = it }
+    }
+}
