8bd918475d84c2f311069466e4aceccd397f70c9
Ercan Duman <ercan.duman@macellan.net>
2021-07-10 14:56:31 +0300

    ViewBinding feature and view binding implementation for MainActivity.kt added

--- app/src/main/java/ercanduman/csvparsingdemo/ui/MainActivity.kt
+++ app/src/main/java/ercanduman/csvparsingdemo/ui/MainActivity.kt
@@ -3,16 +3,18 @@ package ercanduman.csvparsingdemo.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import ercanduman.csvparsingdemo.R
 import ercanduman.csvparsingdemo.data.source.LocalDataSource
+import ercanduman.csvparsingdemo.databinding.ActivityMainBinding
 import ercanduman.csvparsingdemo.util.Resource
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val factory = MainViewModelFactory(LocalDataSource(this))
         viewModel = ViewModelProvider(this, factory).get(MainViewModel::class.java)
