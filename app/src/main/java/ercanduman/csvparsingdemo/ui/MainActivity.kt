14333f85862adace6f7e5c85b35c2ee1e84bfc75
Ercan Duman <ercan.duman@macellan.net>
2021-07-11 11:15:43 +0300

    No data found case handled.

--- app/src/main/java/ercanduman/csvparsingdemo/ui/MainActivity.kt
+++ app/src/main/java/ercanduman/csvparsingdemo/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
+import ercanduman.csvparsingdemo.R
 import ercanduman.csvparsingdemo.data.model.Issue
 import ercanduman.csvparsingdemo.data.source.LocalDataSource
 import ercanduman.csvparsingdemo.databinding.ActivityMainBinding
@@ -14,13 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
-    private lateinit var issueAdapter: IssueAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        issueAdapter = IssueAdapter()
         val factory = MainViewModelFactory(LocalDataSource(this))
         viewModel = ViewModelProvider(this, factory).get(MainViewModel::class.java)
 
@@ -31,14 +30,14 @@ class MainActivity : AppCompatActivity() {
         viewModel.getData()
         viewModel.resourceLiveData.observe(this) { resource ->
             when (resource) {
-                is Resource.Error -> handleException(resource.message)
+                is Resource.Error -> displayErrorMessage(resource.message)
                 is Resource.Success -> handleRetrievedData(resource.issues)
                 is Resource.Loading -> binding.mainProgressBar.isVisible = true
             }.exhaustive
         }
     }
 
-    private fun handleException(message: String) {
+    private fun displayErrorMessage(message: String) {
         binding.apply {
             mainProgressBar.isVisible = false
             mainErrorMessage.apply {
@@ -49,9 +48,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun handleRetrievedData(issues: List<Issue>) {
-        issueAdapter.submitList(issues)
+        if (issues.isEmpty()) {
+            displayErrorMessage(getString(R.string.no_data_found))
+            return
+        }
+
         binding.apply {
             mainProgressBar.isVisible = false
+
+            val issueAdapter = IssueAdapter().apply { submitList(issues) }
+
             mainRecyclerView.apply {
                 isVisible = true
                 setHasFixedSize(true) // Added for better performance optimizations.
