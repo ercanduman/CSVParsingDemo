3154aad6ecded7f8e604915414f14bbbdc6e6aa0
Ercan Duman <ercan.duman@macellan.net>
2021-07-11 10:50:32 +0300

    IssueAdapter.kt created to adapt every issue item to layout_list_item_issue.xml layout

---
+++ app/src/main/java/ercanduman/csvparsingdemo/ui/IssueAdapter.kt
@@ -0,0 +1,63 @@
+package ercanduman.csvparsingdemo.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import ercanduman.csvparsingdemo.R
+import ercanduman.csvparsingdemo.data.model.Issue
+import ercanduman.csvparsingdemo.databinding.LayoutListItemIssueBinding
+
+/**
+ * Adapter is a class that adapts every item to a list viewHolder and responsible for providing
+ * views that represent items in a data set.
+ *
+ * ListAdapter is base class of RecyclerView.Adapter for presenting List data in a RecyclerView,
+ * including computing diffs between Lists on a background thread. While using a LiveData<List> is
+ * an easy way to provide data to the adapter, it isn't required - you can use submitList(List) when
+ * new lists are available.
+ *
+ * @author ercanduman
+ * @since  11.07.2021
+ */
+class IssueAdapter : ListAdapter<Issue, IssueAdapter.IssueViewHolder>(ISSUE_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IssueViewHolder {
+        val binding: LayoutListItemIssueBinding =
+            LayoutListItemIssueBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return IssueViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: IssueViewHolder, position: Int) {
+        val currentIssue = getItem(position)
+        if (currentIssue != null) {
+            holder.bindIssue(currentIssue)
+        }
+    }
+
+    class IssueViewHolder(private val binding: LayoutListItemIssueBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bindIssue(issue: Issue) {
+            binding.apply {
+                itemBirthDate.text = issue.dateOfBirth
+                itemIssueCount.text = issue.issueCount.toString()
+
+                val firstNameAndSurname = binding.root.context.getString(
+                    R.string.format_first_name_and_surname,
+                    issue.firstname,
+                    issue.surname
+                )
+                itemFirstNameAndSurname.text = firstNameAndSurname
+            }
+        }
+    }
+
+    companion object {
+        private val ISSUE_COMPARATOR = object : DiffUtil.ItemCallback<Issue>() {
+            override fun areItemsTheSame(oldItem: Issue, newItem: Issue): Boolean = oldItem == newItem
+            override fun areContentsTheSame(oldItem: Issue, newItem: Issue): Boolean = oldItem == newItem
+        }
+    }
+}
