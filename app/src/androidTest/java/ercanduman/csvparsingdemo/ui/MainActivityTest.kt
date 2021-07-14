f31161b9e9263b3781819348ffe8f3358e55b903
Ercan Duman <ercan.duman@macellan.net>
2021-07-14 12:20:49 +0300

    Displaying items based on file content test cases updated for MainActivityTest.kt

--- app/src/androidTest/java/ercanduman/csvparsingdemo/ui/MainActivityTest.kt
+++ app/src/androidTest/java/ercanduman/csvparsingdemo/ui/MainActivityTest.kt
@@ -1,10 +1,17 @@
 package ercanduman.csvparsingdemo.ui
 
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.google.common.truth.Truth.assertThat
 import ercanduman.csvparsingdemo.R
+import ercanduman.csvparsingdemo.data.source.LocalDataSource
+import ercanduman.csvparsingdemo.util.Resource
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 
@@ -37,9 +44,38 @@ class MainActivityTest {
     }
 
     @Test
-    fun test_launchMainActivity_and_checkIfChildViewsAreAvailableAndDisplayedProperly() {
-        onView(withId(R.id.main_recycler_view)).check(matches(isDisplayed()))
-        onView(withId(R.id.main_progress_bar)).check(matches(withEffectiveVisibility(Visibility.GONE)))
-        onView(withId(R.id.main_error_message)).check(matches(withEffectiveVisibility(Visibility.GONE)))
-    }
+    fun test_launchMainActivity_and_checkIfChildViewsAreAvailableAndDisplayedProperly(): Unit =
+        runBlocking {
+            val localDataSource = LocalDataSource(ApplicationProvider.getApplicationContext())
+            val resourceList = localDataSource.readFile().toList()
+
+            // The first emitted value is always Resource.Loading.
+            assertThat(resourceList[0] is Resource.Loading)
+
+            // Second emitted value can be Resource.Success or Resource.Error
+            // To collect second emitted flow value, toList()[1] should be used.
+            when (val resource = resourceList[1]) {
+                is Resource.Error -> {
+                    onView(withId(R.id.main_progress_bar)).check(matches(not(isDisplayed())))
+                    onView(withId(R.id.main_recycler_view)).check(matches(not(isDisplayed())))
+                    onView(withId(R.id.main_error_message)).check(matches(isDisplayed()))
+                }
+                is Resource.Loading -> {
+                    onView(withId(R.id.main_progress_bar)).check(matches(isDisplayed()))
+                    onView(withId(R.id.main_recycler_view)).check(matches(not(isDisplayed())))
+                    onView(withId(R.id.main_error_message)).check(matches(not(isDisplayed())))
+                }
+                is Resource.Success -> {
+                    if (resource.issues.isNotEmpty()) {
+                        onView(withId(R.id.main_progress_bar)).check(matches(not(isDisplayed())))
+                        onView(withId(R.id.main_recycler_view)).check(matches(isDisplayed()))
+                        onView(withId(R.id.main_error_message)).check(matches(not(isDisplayed())))
+                    } else {
+                        onView(withId(R.id.main_progress_bar)).check(matches(not(isDisplayed())))
+                        onView(withId(R.id.main_recycler_view)).check(matches(not(isDisplayed())))
+                        onView(withId(R.id.main_error_message)).check(matches(isDisplayed()))
+                    }
+                }
+            }
+        }
 }
