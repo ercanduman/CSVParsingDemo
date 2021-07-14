7826b6a35c4a9dca4d503d3fbde90d72b3c34ab5
Ercan Duman <ercan.duman@macellan.net>
2021-07-14 12:56:37 +0300

    Displaying NoDataFound message test cases added for MainActivityTest.kt

--- app/src/androidTest/java/ercanduman/csvparsingdemo/ui/MainActivityTest.kt
+++ app/src/androidTest/java/ercanduman/csvparsingdemo/ui/MainActivityTest.kt
@@ -55,27 +55,59 @@ class MainActivityTest {
             // Second emitted value can be Resource.Success or Resource.Error
             // To collect second emitted flow value, toList()[1] should be used.
             when (val resource = resourceList[1]) {
-                is Resource.Error -> {
-                    onView(withId(R.id.main_progress_bar)).check(matches(not(isDisplayed())))
-                    onView(withId(R.id.main_recycler_view)).check(matches(not(isDisplayed())))
-                    onView(withId(R.id.main_error_message)).check(matches(isDisplayed()))
-                }
-                is Resource.Loading -> {
-                    onView(withId(R.id.main_progress_bar)).check(matches(isDisplayed()))
-                    onView(withId(R.id.main_recycler_view)).check(matches(not(isDisplayed())))
-                    onView(withId(R.id.main_error_message)).check(matches(not(isDisplayed())))
-                }
+                is Resource.Error -> checkErrorViews()
+                is Resource.Loading -> checkLoadingViews()
                 is Resource.Success -> {
                     if (resource.issues.isNotEmpty()) {
-                        onView(withId(R.id.main_progress_bar)).check(matches(not(isDisplayed())))
-                        onView(withId(R.id.main_recycler_view)).check(matches(isDisplayed()))
-                        onView(withId(R.id.main_error_message)).check(matches(not(isDisplayed())))
+                        checkSuccessViews()
                     } else {
-                        onView(withId(R.id.main_progress_bar)).check(matches(not(isDisplayed())))
-                        onView(withId(R.id.main_recycler_view)).check(matches(not(isDisplayed())))
-                        onView(withId(R.id.main_error_message)).check(matches(isDisplayed()))
+                        checkErrorViews()
                     }
                 }
             }
         }
+
+    @Test
+    fun test_launchMainActivity_and_checkIfNoDataFoundMessageDisplayedForEmptyFile(): Unit =
+        runBlocking {
+            val localDataSource = LocalDataSource(ApplicationProvider.getApplicationContext())
+            val resourceList = localDataSource.readFile().toList()
+
+            // The first emitted value is Resource.Loading
+            assertThat(resourceList[0] is Resource.Loading)
+
+            // Second emitted value can be Resource.Error or Resource.Success based on reading file execution
+            when (val resource: Resource = resourceList[1]) {
+                is Resource.Error -> checkErrorViews()
+                is Resource.Loading -> checkLoadingViews()
+                is Resource.Success -> {
+                    if (resource.issues.isEmpty()) {
+                        checkErrorViews()
+
+                        // Check correct error message
+                        onView(withText(R.string.no_data_found)).check(matches(isDisplayed()))
+                    } else {
+                        checkSuccessViews()
+                    }
+                }
+            }
+        }
+
+    private fun checkSuccessViews() {
+        onView(withId(R.id.main_progress_bar)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.main_recycler_view)).check(matches(isDisplayed()))
+        onView(withId(R.id.main_error_message)).check(matches(not(isDisplayed())))
+    }
+
+    private fun checkLoadingViews() {
+        onView(withId(R.id.main_progress_bar)).check(matches(isDisplayed()))
+        onView(withId(R.id.main_recycler_view)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.main_error_message)).check(matches(not(isDisplayed())))
+    }
+
+    private fun checkErrorViews() {
+        onView(withId(R.id.main_progress_bar)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.main_recycler_view)).check(matches(not(isDisplayed())))
+        onView(withId(R.id.main_error_message)).check(matches(isDisplayed()))
+    }
 }
